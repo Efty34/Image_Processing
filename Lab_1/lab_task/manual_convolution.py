70f12f857cb4c93af378b37b7535b2cd5e76714f
Efty34 <efty17.hasan@gmail.com>
2025-08-08 14:10:55 +0600

    fixed

--- Lab_1/lab_task/manual_convolution.py
+++ Lab_1/lab_task/manual_convolution.py
@@ -10,9 +10,6 @@ h, w = box.shape
 
 # Task - 1 : Working with Kernel 1
 
-# Add borders to the images. For the purpose of padding
-box_bordered = cv2.copyMakeBorder(box, 2, 2, 2, 2, cv2.BORDER_CONSTANT)
-
 # Gaussian kernel
 kernel1 = np.array([
     [1,  4,  6,  4, 1],
@@ -22,10 +19,16 @@ kernel1 = np.array([
     [1,  4,  6,  4, 1]
 ], dtype=np.float32)
 
-# flipping the kernel for convolution
+kh, kw = kernel1.shape
+
+# flipping the kernel for pure convolution, if not flipped then that will be cross-correlation.
 kernel1 = np.flip(kernel1)
-# Calculate the center of the kernel
-k_center = kernel1.shape[0] // 2
+
+# we want same output as input, so we need to pad the input image, this formula only works for symmetric kernels
+padding = (kernel1.shape[0] - 1 ) // 2
+
+# Add borders to the images. For the purpose of padding
+box_bordered = cv2.copyMakeBorder(box, padding, padding, padding, padding, cv2.BORDER_CONSTANT)
 
 # output place holder for the convolution
 box_conv = np.zeros_like(box_bordered, dtype=np.float32)
@@ -34,12 +37,12 @@ box_conv = np.zeros_like(box_bordered, dtype=np.float32)
 # img_conv = cv2.filter2D(lena_bordered, ddepth=cv2.CV_32F, kernel= kernel1)
 
 # Manual convolution operation using kernel1
-for i in range(k_center, h + k_center):
-    for j in range(k_center, w + k_center):
+for i in range(padding, h + padding):
+    for j in range(padding, w + padding):
         result = 0
-        for m in range(kernel1.shape[0]):
-            for n in range(kernel1.shape[1]):
-                result += kernel1[m, n] * box_bordered[i - k_center + m, j - k_center + n]
+        for m in range(kh):
+            for n in range(kw):
+                result += kernel1[m, n] * box_bordered[i - padding + m, j - padding + n]
         box_conv[i, j] = result
 
 # Normalize the float result to 0–255 and convert to uint8
@@ -58,8 +61,6 @@ cv2.imshow('Normalized Cropped Image', box_norm_cropped)
 
 # Taks - 2 : Working with Kernel 2
 
-box_bordered_2 = cv2.copyMakeBorder(box, 2, 0, 2, 0, cv2.BORDER_CONSTANT)
-
 # In kernel 2, the center is at (0, 0) or top left corner
 kernel2 = np.array([[-1, 0, 1],
                     [-1, 0, 1],
@@ -68,19 +69,22 @@ kernel2 = np.array([[-1, 0, 1],
 # flipping the kernel for convolution
 kernel2 = np.flip(kernel2)
 
+kh2, kw2 = kernel2.shape
+
+# Padding for kernel2, no formula can be applied, intution only
+box_bordered_2 = cv2.copyMakeBorder(box, 2, 0, 2, 0, cv2.BORDER_CONSTANT)
+
 box_conv_2 = np.zeros_like(box_bordered_2, dtype=np.float32)
 
-# Manual convolution operation using kernel2 with center at (0,0)
-k_center_2 = 0  # Center at top-left corner
 
-for i in range(h):
-    for j in range(w):
+
+for i in range(2, h+2):
+    for j in range(2, w+2):
         result = 0
-        for m in range(kernel2.shape[0]):
-            for n in range(kernel2.shape[1]):
-                # Kernel center at (0,0) means we access from current position onward
-                result += kernel2[m, n] * box_bordered_2[i + m, j + n]
-        box_conv_2[i + 2, j+2] = result
+        for m in range(kh2):
+            for n in range(kw2):
+                result += kernel2[m, n] * box_bordered_2[i+m-2,j+n-2]
+        box_conv_2[i , j] = result
 
 # Normalize and display results for kernel2
 box_norm_2 = np.round(cv2.normalize(box_conv_2, None, 0, 255, cv2.NORM_MINMAX)).astype(np.uint8)
--- Lab_1/provided_code/image_convolution.py
+++ Lab_1/provided_code/image_convolution.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 # Load grayscale image
-img = cv2.imread('lena.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('Lab_1/assets/box.jpg', cv2.IMREAD_GRAYSCALE)
 h, w = img.shape
 # Add border to preserve edge during convolution
 img_bordered = cv2.copyMakeBorder(img, 2, 2, 2, 2, cv2.BORDER_CONSTANT)
@@ -16,6 +16,8 @@ kernel1 = np.array([
     [1,  4,  6,  4, 1]
 ], dtype=np.float32)
 
+kernel1 = np.flip(kernel1)  # Flip the kernel for convolution
+
 '''
 kernel2 = np.array([[-1, 0, 1],
                     [-1, 0, 1],
@@ -28,6 +30,8 @@ kernel3 = np.array([[-1, -1, -1],
 '''
 # Use float32 depth to keep values beyond 0–255
 img_conv = cv2.filter2D(img_bordered, ddepth=cv2.CV_32F, kernel= kernel1)
+
+print(img_conv.shape)
 
 # Normalize the float result to 0–255 and convert to uint8
 norm = np.round(cv2.normalize(img_conv, None, 0, 255, cv2.NORM_MINMAX)).astype(np.uint8)
